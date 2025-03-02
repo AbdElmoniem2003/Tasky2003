1fddba3635c56ad44c9eb1c8319cdb096931d5a8
AbdElmoniem2003 <elmoniemabd0@gmail.com>
2025-03-02 21:39:09 +0200

    ..

--- src/app/pages/start/start.page.ts
+++ src/app/pages/start/start.page.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { DataService } from 'src/core/services/data.service';
 import { FunctionsService } from 'src/core/services/functions.service';
-<<<<<<< HEAD
-=======
 
->>>>>>> 865af61c3778c4f8f8efeff3b6566d08a0a6a4bb
 
 
 @Component({
@@ -20,21 +17,14 @@ export class StartPage implements OnInit {
 
   constructor(private dataService: DataService,
     private navCtrl: NavController,
-    private funcService:FunctionsService
+    private funcService: FunctionsService
   ) { }
 
 
-<<<<<<< HEAD
   async ngOnInit() {
-     await this.funcService.handleStatusBar('dark', true)
-  }
-=======
-
-  async ngOnInit() {
-     await this.funcService.handleStatusBar('dark', true)
+    await this.funcService.handleStatusBar('dark', true)
   }
 
->>>>>>> 865af61c3778c4f8f8efeff3b6566d08a0a6a4bb
 
   start() {
     this.navCtrl.navigateForward("/login")
